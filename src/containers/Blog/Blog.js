930ae2995da409ab38715be68b2bdeebffa2d629
Maria Ivanova <ivanova.m241@gmail.com>
2020-01-14 10:01:37 +0300

    add nesting routes and redirects

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
 // import axios from 'axios'
-import { Route, NavLink } from 'react-router-dom'
+import { Route, NavLink, Switch, Redirect } from 'react-router-dom'
 import './Blog.css'
 import Posts from './Posts/Posts'
 import NewPost from './NewPost/NewPost'
-import FullPost from './FullPost/FullPost'
+// import FullPost from './FullPost/FullPost'
 
 class Blog extends Component {
   render () {
@@ -14,13 +14,13 @@ class Blog extends Component {
           <nav>
             <ul>
               <li><NavLink
-                to='/'
+                to='/posts'
                 exact
                 // activeClassName='my-active'
                 activeStyle={{
                   color: '#fa923f',
                   textDecoration: 'underline'
-                }}>Home</NavLink></li>
+                }}>Posts</NavLink></li>
               <li><NavLink to={{
                 pathname: '/new-post',
                 hash: '#submit',
@@ -29,10 +29,12 @@ class Blog extends Component {
             </ul>
           </nav>
         </header>
-        <Route path='/' exact component={Posts} />
-        <Route path='/new-post' component={NewPost} />
-        <Route path='/:id' exact component={FullPost} />
-        {/* <Route path='/' component={Posts} /> */}
+        <Switch>
+          <Route path='/new-post' component={NewPost} />
+          <Route path='/posts' component={Posts} />
+          <Redirect from='/' to='/posts' />
+          {/* <Route path='/' component={Posts} /> */}
+        </Switch>
       </div>
     )
   }
--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react'
+import { Route } from 'react-router-dom'
 import Post from '../../../components/Post/Post'
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
 import axios from '../../../axios'
+import FullPost from '../FullPost/FullPost'
 import './Posts.css'
 
 class Posts extends Component {
@@ -31,7 +33,8 @@ class Posts extends Component {
   }
 
   postSelectedHandler = (id) => {
-    this.setState({selectedPostId: id})
+    // this.props.history.push({ pathname: '/posts/' + id })
+    this.props.history.push('/posts/' + id )
   }
 
   render () {
@@ -39,18 +42,22 @@ class Posts extends Component {
     if (!this.state.error) {
       posts = this.state.posts.map(post => {
         return (
-        <Link to={'/' + post.id} key={post.id}>
+        // <Link to={'/posts' + post.id} key={post.id}>
           <Post
+            key={post.id}
             title={post.title}
             author={post.author}
             clicked={() => this.postSelectedHandler(post.id)}/>
-        </Link>
+        // </Link>
       )})
     }
     return (
-      <section className='Posts'>
-        {posts}
-      </section>
+      <div>
+        <section className='Posts'>
+          {posts}
+        </section>
+        <Route path={this.props.match.url + '/:id'} exact component={FullPost} />
+      </div>
     )
   }
 }
