d394ef924ef42cc1185e92bf2981d21c6b4cf028
Maria Ivanova <ivanova.m241@gmail.com>
2020-01-17 09:34:23 +0300

    add lazy loading

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -3,10 +3,17 @@ import React, { Component } from 'react'
 import { Route, NavLink, Switch, Redirect } from 'react-router-dom'
 import './Blog.css'
 import Posts from './Posts/Posts'
-import NewPost from './NewPost/NewPost'
-// import FullPost from './FullPost/FullPost'
+// import NewPost from './NewPost/NewPost'
+import asyncComponent from '../../hoc/asyncComponent'
+const AsyncNewPost = asyncComponent(() => {
+  return import('./NewPost/NewPost')
+})
 
 class Blog extends Component {
+  state = {
+    auth: true
+  }
+
   render () {
     return (
       <div className='Blog'>
@@ -30,9 +37,10 @@ class Blog extends Component {
           </nav>
         </header>
         <Switch>
-          <Route path='/new-post' component={NewPost} />
+          {this.state.auth ? <Route path='/new-post' component={AsyncNewPost} /> : null}
           <Route path='/posts' component={Posts} />
-          <Redirect from='/' to='/posts' />
+          <Route  render={() => <h1>Not found</h1>} />
+          {/* <Redirect from='/' to='/posts' /> */}
           {/* <Route path='/' component={Posts} /> */}
         </Switch>
       </div>
--- src/containers/Blog/NewPost/NewPost.js
+++ src/containers/Blog/NewPost/NewPost.js
@@ -12,6 +12,7 @@ class NewPost extends Component {
   }
 
   componentDidMount () {
+    // this.props.history.replace('/posts')
     console.log(this.props)
   }
 
@@ -24,7 +25,8 @@ class NewPost extends Component {
     axios.post('https://jsonplaceholder.typicode.com/posts', data)
       .then(response => {
         console.log(response)
-        this.setState({submitted: true})
+        // this.setState({submitted: true})
+        this.props.history.push('/posts')
       })
   }
 
